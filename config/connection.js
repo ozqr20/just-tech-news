c52051039a0f145126b1c505c8e3f0789d744c7d
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-10-19 21:57:48 -0400

    commit routes and models

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,13 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+let sequelize;
+
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize(
+if(process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}else {
+    sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER,
     process.env.DB_PASSWORD, 
@@ -12,7 +17,7 @@ const sequelize = new Sequelize(
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
-    }
-);
+    });
+}
 
 module.exports = sequelize;
